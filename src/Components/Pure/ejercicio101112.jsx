239bf0742d506e294aeedc0fe8835e70be805f6a
AlinaB <bernardezalina@gmail.com>
2023-03-16 13:57:32 +0100

    Corrección ejercicio

--- src/Components/Pure/ejercicio101112.jsx
+++ src/Components/Pure/ejercicio101112.jsx
@@ -4,28 +4,15 @@ import React, { useState } from 'react';
 let red = 0;
 let green = 0;
 let blue = 0;
-
-const firstStyle = {
-    width: '255px',
-    height: '255px',
-    margin: '50px',
-    backgroundColor: 'black',
-    borderRadius: '10px'
-}
-const randomStyle = {
-    width: '255px',
-    height: '255px',
-    margin: '50px',
-    backgroundColor: generateColor(),
-    borderRadius: '10px'
-}
+let color = `rgb(${red},${green},${blue})`
 
 function generateColor() {
-    red = Math.round(Math.random() * 255);
-    green = Math.round(Math.random() * 255);
-    blue = Math.round(Math.random() * 255);
+    red = Math.round(Math.random() * 256);
+    green = Math.round(Math.random() * 256);
+    blue = Math.round(Math.random() * 256);
     return `rgb(${red},${green},${blue})`;
 }
+
 
 const Rectangulo = ({mouseAction, propStyle}) => {
     return(
@@ -35,12 +22,31 @@ const Rectangulo = ({mouseAction, propStyle}) => {
 
 const ComponenteRectangulo = () => {
 
+    const firstStyle = {
+        width: '255px',
+        height: '255px',
+        margin: '50px',
+        backgroundColor: 'black',
+        borderRadius: '10px'
+    }
+
+    const [colorNow, setColor] = useState(color);
     const [styleNow, setStyle] = useState(firstStyle);
 
+    const randomStyle = {
+        width: '255px',
+        height: '255px',
+        margin: '50px',
+        backgroundColor: colorNow,
+        borderRadius: '10px'
+    }
+    
     function mouseAction(e) {
+        let newColor;
         e.preventDefault();
+        newColor = generateColor();
+        setColor(newColor);
         setStyle(randomStyle);
-        return;
     }
 
     let rect; 
