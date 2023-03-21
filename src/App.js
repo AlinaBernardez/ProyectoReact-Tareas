3f661a7eb3c03c914fad1c4f1106c9355b31e01a
AlinaB <bernardezalina@gmail.com>
2023-03-21 14:16:59 +0100

    Primer formulario con Formik

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 // import Father from './Components/Container/father';
 // import ComponenteContexto from './Hooks/ejemplo3';
 import TaskListComponent from './Components/Container/task_list';
+import LoginFormik from './Components/Pure/Forms/loginFormik';
 // import GreetingStyled from './Components/Pure/greetingStyled';
 
 
@@ -11,6 +12,7 @@ function App() {
   return (
     <div className="App">
       <TaskListComponent/>
+      <LoginFormik/>
     </div>
   );
 }
